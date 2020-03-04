3978ac35b400ea4fa43a92fce74cb252b0b24a1e
Gustavo Lepri <gustavolepri@gmail.com>
2020-03-04 20:07:09 -0300

    udpated models and changed value to Integer

--- partiu/models.py
+++ partiu/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 from .managers import UserManager
 
@@ -14,6 +15,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255, blank=True, null=True)
     last_name = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=11, blank=True, null=True)
+    is_active = models.BooleanField('Active', default=True)
+    is_staff = models.BooleanField('Is Staff?', default=False)
 
     objects = UserManager()
 
@@ -43,8 +46,14 @@ class Transaction(models.Model):
         related_name='transactions',
         null=True
     )
-    type = models.CharField(max_length=8, choices=TYPES, blank=True, null=True)
-    value = models.CharField(max_length=20, blank=True, null=True)
+    type = models.CharField(max_length=8, choices=TYPES)
+    value = models.IntegerField()
+    created_on = models.DateTimeField('created on', default=timezone.now)
+    updated_on = models.DateTimeField('updated on', default=timezone.now)
+
+    class Meta:
+        verbose_name = 'transaction'
+        verbose_name_plural = 'transactions'
 
     def __str__(self):
         return "Transaction {}".format(self.id)
